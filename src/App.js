f017d6c173df71e9bf743b878a2c745196f4341d
tattu2705 <tattupro2705@gmail.com>
2023-09-19 10:52:28 +0700

    19/9/2023

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from "./Header"
-import data from './data';
-import Card from './Card';
+
 function App() {
-  const cards = data.map((card)=>{
-    return <Card item={card} key={card.id}/>
-  });
   return (
-    <div className="App">
-      <Header/>
-      <div className="items">
-        {cards}
-      </div>
+    <div>
+      
     </div>
+   
   );
 }
 
